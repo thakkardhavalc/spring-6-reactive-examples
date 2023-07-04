5e820dd551dabd6815a168191d328cbc6332aad1
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-04 10:32:38 -0600

    implemented Flux test cases

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -3,7 +3,10 @@ package guru.springframework.spring6reactiveexamples.repositories;
 import guru.springframework.spring6reactiveexamples.domain.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * Created By dhaval on 2023-07-04
@@ -37,5 +40,42 @@ class PersonRepositoryImplTest {
         Mono<Person> personMono = personRepository.getById(1);
 
         personMono.map(Person::getFirstName).subscribe(log::info);
+    }
+
+    @Test
+    void testFluxBlock() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        assert person != null;
+        log.info(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person -> {
+           log.info(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(log::info);
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list -> {
+            list.forEach(person -> log.info(person.getFirstName()));
+        });
     }
 }
